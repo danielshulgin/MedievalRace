300a9edb6211c604d94fd0ec2d42e687c281d3d4
danielshulgin <danielshulgin@users.noreply.github.com>
2019-07-04 15:25:08 +0300

    test

--- MedievalRace/Assets/Scripts/Boat/CannonManager.cs
+++ MedievalRace/Assets/Scripts/Boat/CannonManager.cs
@@ -6,6 +6,11 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using Photon.Pun;
 
+public enum Side
+{
+    Left, Right
+}
+
 public class CannonManager : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
@@ -13,10 +18,32 @@ public class CannonManager : MonoBehaviourPunCallbacks
     public List<GameObject> rightCannons;
     public PanAndZoom panAndZoom;
 
+    #region LineRenderers
+    [SerializeField]
+    private LineRenderer lineRendererUpRight;
+    [SerializeField]
+    private LineRenderer lineRendererUpLeft;
+    [SerializeField]
+    private LineRenderer lineRendererDownRight;
+    [SerializeField]
+    private LineRenderer lineRendererDownLeft;
+    #endregion
+
+    #region Constrains
+    [Tooltip("contr clockwise, from OX")]
+    [SerializeField]
+    private float maxFireAngle = 45;
+    [Tooltip("contr clockwise, from OX")]
+    [SerializeField]
+    private float minAngle = - 45;
+    #endregion
+
     public GameObject cannonballPref;
-    
+    private int linePointCount = 20;
+
     void Start()
     {
+        ChangeTrajectory(Side.Right, Vector3.right);
         if (photonView.IsMine == false || PhotonNetwork.IsConnected == false)
         {
             return;
@@ -27,7 +54,7 @@ public class CannonManager : MonoBehaviourPunCallbacks
 
     public void Fire(Vector2 position)
     {
-        if (position.x < Camera.main.pixelWidth  / 2)
+        if (position.x < Camera.main.pixelWidth / 2)
         {
             foreach (GameObject cannon in leftCannons)
             {
@@ -43,4 +70,54 @@ public class CannonManager : MonoBehaviourPunCallbacks
         }
 
     }
+
+    public void ShowTrajectory(Side side)
+    {
+        if (side == Side.Right)
+        {
+            lineRendererUpRight.enabled = lineRendererDownRight.enabled = true;
+        }
+        else
+        {
+            lineRendererUpLeft.enabled = lineRendererDownLeft.enabled = true;
+        }
+    }
+
+    public void HideTrajectory(Side side)
+    {
+        if (side == Side.Right)
+        {
+            lineRendererUpRight.enabled = lineRendererDownRight.enabled = false;
+        }
+        else
+        {
+            lineRendererUpLeft.enabled = lineRendererDownLeft.enabled = false;
+        }
+    }
+
+    public void ChangeTrajectory(Side side, Vector3 direction)
+    {
+        if (side == Side.Right)
+        {
+            ChangeTrajectory(lineRendererUpRight, lineRendererDownRight, direction);
+        }
+        else
+        {
+            ChangeTrajectory(lineRendererUpLeft, lineRendererDownLeft, direction);
+        }
+    }
+
+    private void ChangeTrajectory(LineRenderer up, LineRenderer down, Vector3 direction)
+    {
+        Vector3[] upPositions = new Vector3[linePointCount];
+        Vector3[] downPositions = new Vector3[linePointCount];
+        for (int i = 0; i < linePointCount; i++)
+        {
+            upPositions[i] = up.gameObject.transform.position +
+                Quaternion.Euler(direction) * new Vector3(i, Mathf.Sin(i), 0f);
+        }
+        up.SetPositions(upPositions);
+        down.SetPositions(upPositions);
+    }
+
 }
